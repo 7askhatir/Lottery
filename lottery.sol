477a7e4d9b088c568b325fa86e6cd06e092838af
Askhatir khalid <57253021+7askhatir@users.noreply.github.com>
2022-05-29 00:30:28 +0100

    new

--- lottery.sol
+++ lottery.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./nft.sol";
 contract Lottery{   
     address public Token;
+    address public addressNft;
     address public owner;
     address[] players;
     address[] tokensForThisMounts;
@@ -27,9 +29,13 @@ contract Lottery{
     Ticket[] tikets;
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerTiketsCount;
-    constructor(address _adreessToken){
+    mapping (address => uint) numberOfTicketCanShared;
+
+    constructor(address _adreessToken,address _adreessNft){
           Token=_adreessToken;
           owner=_msgSender();
+          addressNft=_adreessNft;
+
       }
     modifier onlyOwner() {
         require(_owner() == _msgSender(), "Ownable: caller is not the owner");
@@ -54,10 +60,9 @@ contract Lottery{
     }
     function checkAddressInLotteryMount(address _tokenAddress) public view returns (bool){
         bool check=false;
-        for(uint i=0;i<tokensForThisMounts.length;i++){
+        for(uint i=0;i<tokensForThisMounts.length;i++)
            if(tokensForThisMounts[i]==_tokenAddress)
            check=true;
-        }
         return check;
     }
     function checkUseralreadyParticipatingForThisToken(address _user,address _token) public view returns(bool){
@@ -74,16 +79,30 @@ contract Lottery{
     function _allowance(address _token) public view returns(uint256){
         return ERC20(_token).allowance(_msgSender(),address(this));
     }
-
-    function enterToLuttory(address _tokenAddress) public {
-         require(lotteryState == LOTTERY_STATE.OPEN ,"Lottery Is Closed");
+    function sharedByBalance(address _tokenAddress) public {
          require(checkBalanceToken(_tokenAddress,_msgSender())>0,"Your balance not suffisant");
          require(checkAddressInLotteryMount(_tokenAddress),"this token not in list address for this mounts");
          require(!checkUseralreadyParticipatingForThisToken(_msgSender(),_tokenAddress),"this user is already participating for this token");
          uint256 amount = checkBalanceToken(_tokenAddress,_msgSender());
-        _safeTransferFrom(ERC20(_tokenAddress),_msgSender(),owner,amount);
-        uint256 numberOfTikets=checkBalanceToken(Token,_msgSender())/10**18;
-        for(uint i=0;i<numberOfTikets;i++){
+         _safeTransferFrom(ERC20(_tokenAddress),_msgSender(),owner,amount);
+         uint256 numberOfTikets=checkBalanceToken(Token,_msgSender())/10**18;
+         numberOfTicketCanShared[_msgSender()]=numberOfTikets;
+    }
+        function enterToLuttory(address _tokenAddress,uint _tokenId) public {
+         require(lotteryState == LOTTERY_STATE.OPEN ,"Lottery Is Closed");
+         uint mulNft=10;
+         uint256 numberOfTikets=numberOfTicketCanShared[_msgSender()];
+         if(_tokenId!=0){
+            NFT nftContrat=NFT(addressNft);
+           require(nftContrat.ownerOf(_tokenId)==_msgSender(),"you are not the owner of this Nft");
+           NFT.NftPro memory  nft= nftContrat.getNftProByTokenId(_tokenId);
+           require(nft.numberOfHead>=1,"Not enough hearts");
+           if(nft.rarity==1){
+              mulNft=14;
+           }
+         }
+         uint256 tiketsShared=numberOfTikets*mulNft/10;
+        for(uint i=0;i<tiketsShared;i++){
             tekitId++;
             Ticket memory T = Ticket(tekitId,_msgSender(),_tokenAddress);
             tikets.push(T);
@@ -92,8 +111,7 @@ contract Lottery{
         ownerTiketsCount[_msgSender()]+=numberOfTikets;
         if(!checkUseralreadyParticipating(_msgSender()))
         players.push(_msgSender());
-        
-    
+        numberOfTicketCanShared[_msgSender()]=0;
     }
     function checkUseralreadyParticipating(address _user) public view returns(bool){
         bool check=false;
